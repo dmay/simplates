a22fa4c37bd4a888f3b06e466a21952f6434af36
Dmitriy Maygurov <dmitriy.maygurov@gmail.com>
2013-02-04 13:16:12 +0400

    Basic tests

--- templates.tests/Templates_BasicOperations.cs
+++ templates.tests/Templates_BasicOperations.cs
@@ -6,5 +6,50 @@ namespace templates.tests
     [TestClass]
     public class Templates_BasicOperations
     {
+        [TestMethod]
+        public void Templates_SimpleReplace()
+        {
+            var tokens = new TokensSet()
+                .Add("x1", "Hello")
+                .Add("x2", "world")
+                .Add("em", "!");
+
+            var template = "{{x1}} {{x2}}{{em}}";
+            var rez = Templates.Process(template, tokens);
+
+            Assert.AreEqual("Hello world!", rez);
+        }
+        
+        [TestMethod]
+        public void Templates_NestedReplace()
+        {
+            var tokens = new TokensSet()
+                .Add("x1", "Hello {{x2}}")
+                .Add("x2", "world{{em}}")
+                .Add("em", "!");
+
+            var template = "{{x1}}";
+            var rez = Templates.Process(template, tokens);
+
+            Assert.AreEqual("Hello world!", rez);
+        } 
+
+        [TestMethod]
+        public void Template_FuncTokens()
+        {
+            var tokens = new TokensSet()
+                .Add("x1", "Hello")
+                .Add("x2", "world")
+                .Add("em", "!");
+
+            var funcs = new TokensSet()
+                .Add("upper", s => s.ToUpper())
+                .Add("em", () => "!");
+
+            var template = "{{upper:{{x1}} {{x2}}}}{{em}}";
+            var rez = Templates.Process(template, tokens, funcs);
+
+            Assert.AreEqual("HELLO WORLD!", rez);
+        }
     }
 }
